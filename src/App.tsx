85bfc10183d9cfe94d459a62f2591888f4cba4a7
OM BHARGAV <ombhargav788@gmail.com>
2025-11-06 04:18:30 +0530

    commiting 2

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,10 @@ function App() {
   }, []);
   return loading ? (
     <>
+    <Routes>
+
     <Route path="dashboard" element={<div>Loading...</div>}></Route>
+    </Routes>
     <Loading />
     </>
   ) : (
